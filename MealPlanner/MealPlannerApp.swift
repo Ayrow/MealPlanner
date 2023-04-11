c996057664a528e1892ec3e4bc2e9e5b98413242
Aymeric <aymeric.pilaert@hotmail.fr>
2023-04-11 10:43:56 +0200

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Aymeric Pilaert on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
